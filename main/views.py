ae8c2d62c2808a49cb75938075822672fa40ec11
ferdinand <ferdinandbs77@gmail.com>
2024-09-10 15:13:28 +0700

    Whermst

--- main/views.py
+++ main/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 def show_main(request):
     context = {
         'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'name': 'Ferdinand Bonfilio Simamora',
+        'class': 'KKI'
     }
 
     return render(request, "main.html", context)
